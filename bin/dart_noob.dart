a22b1c52ae15472ff5ecd9e6065753ed709614e0
Philip Skov <psk@bingbong01.local>
2023-08-16 19:06:04 +0200

    yolo

---
+++ bin/dart_noob.dart
@@ -0,0 +1,5 @@
+import 'package:dart_noob/dart_noob.dart' as dart_noob;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_noob.calculate()}!');
+}
---
+++ test/dart_noob_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_noob/dart_noob.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
