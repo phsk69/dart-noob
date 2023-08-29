9f495575ffa86ecb6d51fbee5aa0c0737a939a31
Philip Skov <psk@skovsystems.dk>
2023-08-30 01:24:14 +0200

    More scheming

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 # dart_noob
 
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+## Useful examples
+
+### Return errors by value
+
+```dart
+// Simple demo setup
+import 'package:dartz/dartz.dart';
+
+Either<String, int> divide(int dividend, int divisor) {
+  if (divisor == 0) {
+    return Left("Cannot divide by zero");
+  } else {
+    return Right(dividend ~/ divisor);
+  }
+}
+
+final result = divide(10, 2);
+result.fold(
+  (l) => print('Error: $l'), 
+  (r) => print('Result: $r')
+);
+```
+
+### Return errors by value with Futures
+
+```dart
+// For async shit
+class Result<T> {
+  final T? value;
+  final Object? error;
+
+  bool get isSuccess => error == null;
+  bool get isError => !isSuccess;
+
+  Result.success(this.value) : error = null;
+  Result.error(this.error) : value = null;
+}
+
+Future<Result<String>> fetchSomething() async {
+  try {
+    String result = await someAsyncOperation();
+    return Result.success(result);
+  } catch (e) {
+    return Result.error(e);
+  }
+}
+var result = await fetchSomething();
+if (result.isSuccess) {
+  print(result.value);
+} else {
+  // TODO: Change all this to logging
+  print('Error occurred: ${result.error}');
+}
+```
--- bin/cli_pro.dart
+++ bin/cli_pro.dart
@@ -5,25 +5,33 @@ import 'dart:convert';
 import 'package:logging/logging.dart';
 
 void main(List<String> args) async {
-  // Configure logger
-  Logger.root.level = Level.ALL;
+  final parser = ArgParser()
+    ..addOption('inputfile',
+        abbr: 'i', help: 'Specify the path of the input file')
+    ..addOption('errorlog', abbr: 'e', help: 'Specify the file to write errors')
+    ..addOption('output', abbr: 'o', help: 'Specify the file to write output')
+    ..addOption('mode', abbr: 'm', help: 'Define the mode')
+    ..addOption('logfile', abbr: 'l', help: 'Specify the file to log messages')
+    ..addOption('loglevel',
+        abbr: 'v',
+        help: 'Specify the logging level (default is WARNING)',
+        allowed: ['ALL', 'FINE', 'INFO', 'WARNING', 'SEVERE', 'SHOUT']);
+
+  final parsedArgs = parser.parse(args);
+
+  // Set log level from command line arguments or default to WARNING
+  var logLevel = Level.WARNING;
+  if (parsedArgs['loglevel'] != null) {
+    logLevel = Level.LEVELS.firstWhere(
+        (l) => l.name == parsedArgs['loglevel'].toUpperCase(),
+        orElse: () => Level.WARNING);
+  }
+  Logger.root.level = logLevel;
 
   // Log to stdout by default
   Logger.root.onRecord.listen((record) {
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
-
-  final log = Logger('CLI'); // Create a logger for this class
-
-  final parser = ArgParser()
-    ..addFlag('stdin', negatable: false, help: 'Read input from stdin')
-    ..addOption('inputfile', help: 'Specify the path of the input file')
-    ..addOption('errorlog', help: 'Specify the file to write errors')
-    ..addOption('output', help: 'Specify the file to write output')
-    ..addOption('mode', abbr: 'm', help: 'Define the mode')
-    ..addOption('logfile', help: 'Specify the file to log messages');
-
-  final parsedArgs = parser.parse(args);
 
   // Log to file if option is specified
   if (parsedArgs['logfile'] != null) {
@@ -34,20 +42,35 @@ void main(List<String> args) async {
     });
   }
 
+  final log = Logger('CLI'); // Create a logger for this class
   log.fine('Parsing command line arguments');
 
-  var inputData = !stdin.hasTerminal
-      ? (stdin.readLineSync() ?? 'default_data')
-      : await _fetchDataFromFile(parsedArgs['inputfile'] as String?);
+  StringBuffer inputDataBuffer = StringBuffer();
+
+  if (!stdin.hasTerminal) {
+    await for (var line
+        in stdin.transform(Utf8Decoder()).transform(LineSplitter())) {
+      inputDataBuffer.writeln(line);
+    }
+  } else {
+    inputDataBuffer
+        .write(await _fetchDataFromFile(parsedArgs['inputfile'] as String?));
+  }
+  // TODO: Make default_data a new and more dynamic type of list as default
+  // See task_list and the builder stunts we were doing with charlie
+  var inputData = inputDataBuffer.toString().isEmpty
+      ? 'default_data'
+      : inputDataBuffer.toString();
 
   var result = demoFunction(inputData, parsedArgs['mode']);
 
   if (result.isLeft()) {
     if (parsedArgs['errorlog'] != null) {
-      await File(parsedArgs['errorlog'] as String)
-          .writeAsString(result.fold((l) => l, (r) => r));
+      await File(parsedArgs['errorlog'] as String).writeAsString(
+          result.fold((leftValue) => leftValue, (rightValue) => rightValue));
     } else {
-      stderr.writeln(result.fold((l) => l, (r) => r));
+      stderr.writeln(
+          result.fold((leftValue) => leftValue, (rightValue) => rightValue));
     }
   } else {
     if (parsedArgs['output'] != null) {
