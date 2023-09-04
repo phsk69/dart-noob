f341ea6a091de4e0c52f046acf2a307f20bc0633
Philip Skov <psk@skovsystems.dk>
2023-09-04 16:29:27 +0200

    Moved helper to lib.

--- lib/days/d1/aoc15_d1.dart
+++ lib/days/d1/aoc15_d1.dart
@@ -1,33 +1,5 @@
-import 'dart:convert';
 import 'package:dart_noob/util/file_stuff.dart';
 import 'package:dartz/dartz.dart';
-
-// Function to get input content as a String
-Future<Either<String, String>> getInputContent(
-    String? input, Stream<List<int>>? inputStream) async {
-  if (input == null && inputStream == null) {
-    return Left("Both input and inputStream cannot be null.");
-  }
-
-  if (input != null && inputStream != null) {
-    return Left("Only one source of input is allowed.");
-  }
-
-  if (input != null) {
-    var content = await getFileAsString(input);
-    return Right(content);
-  } else if (inputStream != null) {
-    var content = StringBuffer();
-    await for (var line in inputStream
-        .transform(utf8.decoder)
-        .transform(const LineSplitter())) {
-      content.writeln(line);
-    }
-    return Right(content.toString());
-  }
-
-  return Left("No valid input provided.");
-}
 
 // Function for the first problem
 Future<Either<String, int>> solveAoc15D1P1(
--- lib/util/file_stuff.dart
+++ lib/util/file_stuff.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:dartz/dartz.dart';
 
 // helpers
 Future<List<String>> getInputByLine(String inputPath) async {
@@ -24,6 +25,33 @@ Stream<String> streamLinesFromFile(String inputPath) async* {
       in file.openRead().transform(utf8.decoder).transform(LineSplitter())) {
     yield line;
   }
+}
+
+// Function to get input content as a String
+Future<Either<String, String>> getInputContent(
+    String? input, Stream<List<int>>? inputStream) async {
+  if (input == null && inputStream == null) {
+    return Left("Both input and inputStream cannot be null.");
+  }
+
+  if (input != null && inputStream != null) {
+    return Left("Only one source of input is allowed.");
+  }
+
+  if (input != null) {
+    var content = await getFileAsString(input);
+    return Right(content);
+  } else if (inputStream != null) {
+    var content = StringBuffer();
+    await for (var line in inputStream
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())) {
+      content.writeln(line);
+    }
+    return Right(content.toString());
+  }
+
+  return Left("No valid input provided.");
 }
 
 class FileReader {
