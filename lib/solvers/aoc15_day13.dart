72ef0899883367f038df139310194eff8471d8a7
Philip Skov <psk@skovsystems.dk>
2023-10-01 13:49:02 +0200

    Day13 part 1 solved not integrated.

--- lib/solvers/aoc15_day13.dart
+++ lib/solvers/aoc15_day13.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:trotter/trotter.dart';
 import 'package:dart_noob/util/string_stuff.dart';
 import 'package:dart_noob/factories/solver_factory.dart';
 
@@ -14,64 +15,70 @@ class Day13P1Solver extends AoCSolver {
     try {
       var inputData =
           input?.toString().trim() ?? getStringSync(filePath!).trim();
-      if (inputData.trim().isEmpty) {
+      if (inputData.isEmpty) {
         return Left('Input is empty.');
       }
-      List<Map<String, Object>> guests = []; // Updated type to Object
+
+      List<List<Object>> guests = [];
 
       for (var line in inputData.split('\n').where((line) => line.isNotEmpty)) {
         List<String> words = line.split(' ');
         if (words.length == 11) {
           var guestName = words[0];
-          var neighborName = words[10]
-              .substring(0, words[10].length - 1);
-          var happinessChange = int.parse(words[3]);
+          var happinessChange =
+              words[2] == "lose" ? -int.parse(words[3]) : int.parse(words[3]);
+          var neighborName = words[10].substring(0, words[10].length - 1);
 
-          if (words[2] == 'gain') {
-            if (guests.isEmpty) {
-              guests.add({
-                'guest': guestName,
-                'preferences': <String, int>{neighborName: happinessChange}
-              });
-            } else {
-              var existingGuest = guests.firstWhere(
-                (guest) => guest['guest'] == guestName,
-                orElse: () => {
-                  'guest': guestName,
-                  'preferences': <String, int>{}
-                },
-              );
-
-              if (existingGuest != null) {
-                existingGuest['preferences'][neighborName] = happinessChange;
-              } else {
-                guests.add({
-                  'guest': guestName,
-                  'preferences': <String, int>{neighborName: happinessChange}
-                });
-              }
-            }
-          }
+          guests.add([guestName, happinessChange, neighborName]);
         }
       }
 
-      // Now 'guests' list contains guest information with their preferences
-      // Continue with the rest of your logic
+      var allNames = guests.map((g) => g[0] as String).toSet().toList();
+      var maxHappiness = getMaxHappiness(allNames, guests, []);
 
-      return Right('Day13P1Solver: placeholder');
+      return Right('Day13P2Solver: $maxHappiness');
     } catch (e) {
       return Left(e.toString());
     }
   }
-}
 
-      // Now 'guests' list contains guest information with their preferences
-      // Continue with the rest of your logic
-
-      return Right('Day13P1Solver: placeholder');
-    } catch (e) {
-      return Left(e.toString());
+  int getMaxHappiness(List<String> names, List<List<Object>> relationships,
+      List<String> currentArrangement) {
+    if (names.isEmpty) {
+      return calculateHappinessForArrangement(
+          currentArrangement, relationships);
     }
+
+    int maxHappiness = 0;
+
+    for (var name in names) {
+      var newNames = List.of(names)..remove(name);
+      var newArrangement = List.of(currentArrangement)..add(name);
+      var currentHappiness =
+          getMaxHappiness(newNames, relationships, newArrangement);
+      if (currentHappiness > maxHappiness) {
+        maxHappiness = currentHappiness;
+      }
+    }
+
+    return maxHappiness;
+  }
+
+  int calculateHappinessForArrangement(
+      List<String> arrangement, List<List<Object>> relationships) {
+    int happiness = 0;
+    for (var i = 0; i < arrangement.length; i++) {
+      var currentGuest = arrangement[i];
+      var leftNeighbor =
+          arrangement[(i - 1 + arrangement.length) % arrangement.length];
+      var rightNeighbor = arrangement[(i + 1) % arrangement.length];
+
+      happiness += relationships.firstWhere(
+          (r) => r[0] == currentGuest && r[2] == leftNeighbor)[1] as int;
+      happiness += relationships.firstWhere(
+          (r) => r[0] == currentGuest && r[2] == rightNeighbor)[1] as int;
+    }
+    return happiness;
   }
 }
 
